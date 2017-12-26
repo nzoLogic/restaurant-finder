94c9fff152e8cd8ad0721d8280918f25e5657eb7
nzoLogic <nzoLogic@gmail.com>
2017-12-26 16:17:50 -0500

    render relevant props to UI

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 
 const Results = props => (
-  <Container>
-    Results
-  </Container>
+  <Card fluid>
+    <Card.Content>
+      <Card.Header>
+        <Image floated='left' size='small' src={props.image_url} />
+        { props.name }
+      </Card.Header>
+      <Card.Meta>
+        <p>{ `${props.stars_count} stars (${props.reviews_count} reviews)` }</p>
+        <p>{ `${props.food_type} | ${props.area} | ${props.price_range}`}</p>
+      </Card.Meta>
+    </Card.Content>
+  </Card>
 )
 
 export default Results
